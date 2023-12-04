bb000a0cd3b358450fd9bd5ca38e25fd54515243
roberta-munive <114707010+roberta-munive@users.noreply.github.com>
2023-12-04 12:05:56 -0600

    alternative solution to homework 5

--- lesson_5_js_date_formats/src/index.js
+++ lesson_5_js_date_formats/src/index.js
@@ -1,9 +1,9 @@
-let currentDate = moment().format("[Today is] dddd, MMMM D, YYYY");
+let currentDate = moment();
+let formattedCurrentDate = currentDate.format("dddd, MMMM D, YYYY");
 let currentDateLocator = document.querySelector("#current-date");
-currentDateLocator.innerHTML = currentDate;
+currentDateLocator.innerHTML = `Today is ${formattedCurrentDate}`;
 
-let futureDate = moment()
-  .add(10, "year")
-  .format("[10 years from now, it will be] dddd, MMMM D, YYYY h:mm:ss A");
+let futureDate = moment().add(10, "year");
+let formattedFutureDate = futureDate.format("dddd, MMMM D, YYYY h:mm:ss A");
 let futureDateSelector = document.querySelector("#future-date");
-futureDateSelector.innerHTML = futureDate;
+futureDateSelector.innerHTML = `10 years from now, it will be ${formattedFutureDate} `;
